0504733e2f01ff6cd08f31ffdaad0262370ce147
vafnucb <vafnucb2020@gmail.com>
2023-12-22 17:34:24 -0400

    Probando nuevo script de copia de archivos

---
+++ copyFilesFront.cmd
@@ -0,0 +1,16 @@
+@ECHO OFF
+
+set rootpath=%~dp0
+set destination="C:\Users\Juanpi\Desktop\PruebScripts"
+
+mkdir "%destination%\Static"
+robocopy "%rootpath%\dist\static" "%destination%\Static" /E /COPYALL /is
+
+robocopy "%rootpath%\dist\\" "%destination%\Views\Home\" "index.html" /COPYALL /is
+
+REM Crear el archivo Index.cshtml
+echo @{    Layout = "";   } > "%destination%\Views\Home\Index.cshtml"
+type "%destination%\Views\Home\index.html" >> "%destination%\Views\Home\Index.cshtml"
+
+ECHO ON
+exit /b
