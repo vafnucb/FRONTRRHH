a2b1e880689ae8ef6a3bf79a5aed340d8cfa9c17
vafnucb <vafnucb2020@gmail.com>
2024-01-14 04:56:20 -0400

    .

--- MergeFront.bat
+++ MergeFront.bat
@@ -6,30 +6,28 @@ set EXCLUDED_FILE=src\main.js
 REM Realizar un stash de los cambios locales en la rama de destino (main)
 git stash
 
-REM Realizar el merge
-git merge -s recursive -X theirs dev
+REM Realizar el merge con estrategia "ours" para rechazar los cambios de dev en src\main.js
+git merge -s ours dev --no-commit
+git checkout -- "%EXCLUDED_FILE%"
 
-REM Deshacer los cambios específicos en la rama de destino (main) si hay conflictos
-git checkout --strategy-option=theirs -- "%EXCLUDED_FILE%"
-
-REM Commit de los cambios en la rama de destino (main)
-git add "%EXCLUDED_FILE%"
+REM Commit del merge
+git add .
 git commit -m "Merge de dev en main, excluyendo cambios en %EXCLUDED_FILE%"
 
 REM Aplicar los cambios locales nuevamente (sin aplicar los cambios del archivo específico)
 git stash apply --index
 
-REM Eliminar la carpeta 'dist'
-rmdir /s /q dist
+@REM REM Eliminar la carpeta 'dist'
+@REM rmdir /s /q dist
 
-REM Ejecutar el comando npm run build
-npm run build
+@REM REM Ejecutar el comando npm run build
+@REM npm run build
 
-REM Añadir todos los cambios
-git add .
+@REM REM Añadir todos los cambios
+@REM git add .
 
-REM Hacer el commit
-git commit -m "Build Automático"
+@REM REM Hacer el commit
+@REM git commit -m "Build Automático"
 
-REM Realizar el push
-git push
+@REM REM Realizar el push
+@REM git push
